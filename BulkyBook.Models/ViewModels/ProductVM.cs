cf1b673a83a7fa73995cba04d59b6b919473339b
malik <malek96bk@hotmail.com>
2022-09-19 16:36:56 +0200

    SweatAlerts doen 109

--- BulkyBook.Models/ViewModels/ProductVM.cs
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿//using System.Web.WebPages.Html;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBook.Models.ViewModels
 {
