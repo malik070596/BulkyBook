19ef151407ef5c3e731f34230448625b98bfd800
malik <malek96bk@hotmail.com>
2022-09-21 13:25:04 +0200

    foto

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -92,7 +92,7 @@ namespace BulkyBook.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 string wwwRootPath = _hostEnvironment.WebRootPath;
-                if(file != null)
+                if (file != null)
                 {
                     //Guid.NewGuid its here to rename the foto file so we dont get two files with the samen name!!!
                     string fileName = Guid.NewGuid().ToString();
@@ -100,7 +100,7 @@ namespace BulkyBook.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
-                    if(obj.Product.ImageUrl != null)
+                    if (obj.Product.ImageUrl != null)
                     {
                         var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
                         if (System.IO.File.Exists(oldImagePath))
@@ -115,7 +115,7 @@ namespace BulkyBook.Areas.Admin.Controllers
                     }
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
-                if (obj.Product.Id == 0 )
+                if (obj.Product.Id == 0)
                 {
                     _unitOfWork.Product.Add(obj.Product);
                 }
@@ -125,12 +125,22 @@ namespace BulkyBook.Areas.Admin.Controllers
 
                 }
                 //to update the data in the database and savechanges its to save it
-                
+
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully";
                 return RedirectToAction("Index");
 
             }
+            obj.CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString(),
+            });
+            obj.CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
             return View(obj);
 
         }
@@ -140,7 +150,7 @@ namespace BulkyBook.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productlist = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            var productlist = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = productlist });
         }
         //Post
@@ -152,7 +162,7 @@ namespace BulkyBook.Areas.Admin.Controllers
             var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
             if (obj == null)
             {
-                return Json(new {success =false,message = "Error while deleting"});
+                return Json(new { success = false, message = "Error while deleting" });
             }
             var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
             if (System.IO.File.Exists(oldImagePath))
