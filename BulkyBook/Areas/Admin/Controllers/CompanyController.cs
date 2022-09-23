e5ebdc6548ca97627588b1f41cdee1a4ec613f3b
malik <malek96bk@hotmail.com>
2022-09-23 11:58:15 +0200

    add a Company

---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,129 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Build.Tasks;
+using System.Drawing.Imaging;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //Get
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        //GET
+
+        //to edit one or more items that we have in the database
+
+        public IActionResult Upsert(int? id)
+        {
+            #region
+            //Product product = new();
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+            //    u => new SelectListItem
+            //    {
+            //        Text = u.Name,
+            //        Value = u.Id.ToString()
+            //    });
+            //IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+            //    u => new SelectListItem
+            //    {
+            //        Text = u.Name,
+            //        Value = u.Id.ToString()
+            //    });
+            #endregion
+            Company company = new();
+
+            if (id == null || id == 0)
+            {
+                //Create Product
+                #region
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                //ViewBag.CoverTypleList = CoverTypeList;
+                #endregion
+                return View(company);
+            }
+            else
+            {
+                //Update product
+                company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                return View(company);
+            }
+
+
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // to Edit an category and post it in the database
+        public IActionResult Upsert(Company obj, IFormFile? file)
+        {
+
+            //to check if all the field are true
+            if (ModelState.IsValid)
+            {
+
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company created Successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    TempData["success"] = "Company updated successfully";
+
+                }
+                //to update the data in the database and savechanges its to save it
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+                return View(obj);
+
+            }
+
+
+            #region API CALLS
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+            var companyList = _unitOfWork.Company.GetAll();
+                return Json(new { data = companyList });
+            }
+            //Post
+            [HttpDelete]
+            // to Delete an category and post it in the database
+            public IActionResult Delete(int? id)
+            {
+                //take the id from the data base and if there is no id we will get nothing notfound
+                var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                if (obj == null)
+                {
+                    return Json(new { success = false, message = "Error while deleting" });
+                }
+                
+                //(Remove) to Delete the data in the database and (savechanges) its to save it
+                _unitOfWork.Company.Remove(obj);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Delete Successful" });
+
+            }
+            #endregion
+        }
+
+    }
---
+++ BulkyBook/wwwroot/js/company.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#Table').DataTable({
+        "ajax": {
+            "url": "/Admin/Company/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "streetAddress", "width": "15%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-100 btn-group" role="group">
+                          <a href="/Admin/Company/Upsert?id=${data}"
+                          class="btn btn-primary mx-1"><i class="bi bi-plus-circle"></i>&nbsp Edit</a>
+                          <a onClick=Delete('/Admin/Company/Delete/${data}')
+                          class="btn btn-danger mx-1"><i class="bi bi-trash3"></i>&nbsp Delete</a>
+                        </div>
+                    `
+                },
+                "width": "15%"
+            },
+        ]
+    });
+}
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You are about to delete this Company!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+
+                }
+            })
+
+        }
+    })
+}
