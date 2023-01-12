118cc8d990e673bfa891e8325c8b12dc15cce501
malik <malek96bk@hotmail.com>
2023-01-12 10:15:10 +0100

    orders

---
+++ BulkyBook.DataAccess/Migrations/20230106104311_bulkybook.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class bulkybook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentIntentId",
+                table: "OrderHeaders",
+                newName: "PaymentItentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentItentId",
+                table: "OrderHeaders",
+                newName: "PaymentIntentId");
+        }
+    }
+}
